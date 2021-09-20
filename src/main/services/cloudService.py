d01a3c5a7c637978742e6e1c57bb4bc69811861a
Groupe2filRouge <86656683+Groupe2filRouge@users.noreply.github.com>
2021-09-20 23:10:30 +0200

    Update cloudService.py

--- src/main/services/cloudService.py
+++ src/main/services/cloudService.py
@@ -36,7 +36,12 @@ class CloudService():
         except ClientError:
             # The bucket does not exist or you have no access.
             print("client exception")
-        self.s3_client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={'LocationConstraint': 'eu-west-3'}) #commentée pour éviter d'avoir à supprimer le bucket à chaque fois.
+        self.s3_client.create_bucket(
+            Bucket=bucket_name, 
+            CreateBucketConfiguration={
+                'LocationConstraint': 'af-south-1'|'ap-east-1'|'ap-northeast-1'|'ap-northeast-2'|'ap-northeast-3'|'ap-south-1'|'ap-southeast-1'|'ap-southeast-2'|'ca-central-1'|'cn-north-1'|'cn-northwest-1'|'EU'|'eu-central-1'|'eu-north-1'|'eu-south-1'|'eu-west-1'|'eu-west-2'|'eu-west-3'|'me-south-1'|'sa-east-1'|'us-east-2'|'us-gov-east-1'|'us-gov-west-1'|'us-west-1'|'us-west-2'
+                },
+        ) #commentée pour éviter d'avoir à supprimer le bucket à chaque fois.
         website_configuration = {'IndexDocument': {'Suffix': 'README.html'}}
         self.s3_client.put_bucket_website(Bucket=bucket_name, WebsiteConfiguration=website_configuration)
         bucket_policy = {'Version': '2012-10-17','Statement': [{'Sid': 'AddPerm','Effect': 'Allow','Principal': '*','Action': ['s3:GetObject'],'Resource': "arn:aws:s3:::%s/*" % bucket_name}]}
